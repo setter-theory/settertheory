0edd41f01fc62405698e49f6add522346455d5c2
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-10 20:24:27 +0900

    V63

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='setter-theory-v62-native-input-finish';
+const CACHE_NAME = 'setter-theory-v63';
 const ASSETS=['./','index.html','app.js?v=62-native-input-finish','manifest.json','sw.js','icons/aquila-192.png','icons/aquila-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
