f8343bf2ab3eac5ed2d18218e1d7731569f42f06
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-17 23:25:49 +0900

    V107

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'setter-theory-v1063-ranking-order-fix';
+const CACHE_NAME = 'setter-theory-v107-setter-iq-radar-chart';
 const ASSETS=['./','./index.html','./app.js?v=1063-ranking-order-fix','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
