bd3bd68b398e20f7622f0379475b35f781e51fa7
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-18 12:55:37 +0900

    V135

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'setter-theory-v135-play-success-bars';
-const ASSETS=['./','./index.html','./app.js?v=125-home-cleanup','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const ASSETS=['./','./index.html','./app.js?v=135-play-success-bars-fix','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
