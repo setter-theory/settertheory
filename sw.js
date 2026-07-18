45c9968e369946a94e149b93808cc61ef6393901
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-18 13:14:44 +0900

    V135

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'setter-theory-v135-corrected-2';
-const ASSETS=['./','./index.html','./app_v135_corrected.js?v=135-corrected-2','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'setter-theory-v135-saved-report-fix';
+const ASSETS=['./','./index.html','./app_v135_saved_report_fix.js?v=135-saved-report-fix','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
