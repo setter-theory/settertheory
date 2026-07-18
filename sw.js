0d7cb6e4226d650234b1a872bdb02b5eab7e0bc0
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-18 11:47:52 +0900

    V128

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'setter-theory-v127-csv-table-text-fix';
+const CACHE_NAME = 'setter-theory-v128-log-table-stability-fix';
 const ASSETS=['./','./index.html','./app.js?v=125-home-cleanup','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
