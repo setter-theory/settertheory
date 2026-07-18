6d60f73abd85b336310a169cbcee9f7a4c210f2a
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-18 13:29:06 +0900

    V137

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'setter-theory-v136-result-ratio-bars';
-const ASSETS=['./','./index.html','./app_v136_result_ratio_bars.js?v=136-result-ratio-bars','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'setter-theory-v137-rotation-point-diverging-bars';
+const ASSETS=['./','./index.html','./app_v137_rotation_point_diverging_bars.js?v=137-rotation-point-diverging-bars','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
