096665fc942b330c5c09bdfa9d65dad9afcad0cb
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-20 23:56:53 +0900

    V150-16

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'settertheory-v15014-pdf-recovery';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=15015','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=15016','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
