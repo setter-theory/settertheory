11d88fdcfc68eff7abfde3004c5d36c0d2ac843e
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-20 15:24:51 +0900

    V146文字色修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'setter-theory-v146-play-metrics-bars-1';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=146-play-metrics-1','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'setter-theory-v146-play-metrics-bars-2';
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=146-play-metrics-bars-2','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
