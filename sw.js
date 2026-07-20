2fc9756837b2b23f8ef1b64e10ddb08c42659105
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-20 17:39:04 +0900

    V146チーム分析修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'settertheory-v146-rankings-log-accordion-1';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=146-rankings-log-accordion-1','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'settertheory-v146-team-analysis-height-compact-1';
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=146-team-analysis-height-compact-1','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
