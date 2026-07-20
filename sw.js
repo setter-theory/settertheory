a0ac79a498d3f0201d7fa4b1971b871fa24ed4d3
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-20 20:23:12 +0900

    V146ロゴ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v146-setter-aquila-coach';
+const CACHE_NAME = 'settertheory-v146-aquila-iq-logo';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=146-team-aquila-coach-1','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
