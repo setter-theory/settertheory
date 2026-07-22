68028b5b3c36609fcb0cd3d96871d949fee765bd
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-22 18:11:23 +0900

    V150-58

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'settertheory-v15057-setter-analysis-full-width';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=15057','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'settertheory-v15055-pdf-rotation-count-extra-clearance';
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=15055','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
