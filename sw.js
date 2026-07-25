e6f36fe1a604035d105ae6eeb2e0c212b6f7d703
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-26 00:43:09 +0900

    V150-184

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'settertheory-v150183-deferred-logo-mount';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150183','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'settertheory-v150184-report-screen-fix';
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150184','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
