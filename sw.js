ccba264b13b009a8ce99a08300ef2931dc1d45e5
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-25 22:54:33 +0900

    V150-175

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'settertheory-v150174-real-aquila';
+const CACHE_NAME = 'settertheory-v150175-launch-visibility-fix';
 const ASSETS=[
-  './assets/aquila_real.webp','./','./index.html','./app_v141_setter_split.js?v=150174','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+  './assets/aquila_real.webp','./','./index.html','./app_v141_setter_split.js?v=150175','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
