b5b36594caa564f5ee43a94bc903e0d158a446c4
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-27 20:04:21 +0900

    V150-229

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v150-228-coach-board';
+const CACHE_NAME = 'settertheory-v150-229-coach-board';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150208','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
