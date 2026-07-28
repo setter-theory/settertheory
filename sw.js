4205b9c12fa2f92c01a0608de69369d64f6f066c
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-07-28 20:12:33 +0900

    V150-249

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v150-245-coach-flip-all-drawings-fix';
+const CACHE_NAME = 'settertheory-v150-249-coach-direct-court-list';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150208','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
