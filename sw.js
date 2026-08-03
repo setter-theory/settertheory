362ba969ea49b588133ac9505c62c593fb4f2323
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-08-03 20:46:25 +0900

    V150.297

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'settertheory-v150-296-coach-card-fit-initial-sync';
-const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150293','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
+const CACHE_NAME = 'settertheory-v150-297-coach-lineup-standard-order';
+const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=150297','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{
