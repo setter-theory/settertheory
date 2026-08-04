4dcdee639d3db57af5aa053477530e5bbced8cf6
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-08-04 16:44:19 +0900

    V151.1.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v151-1-3-coach-athlete-ui';
+const CACHE_NAME = 'settertheory-v151-1-4-coach-athlete-ui';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=151130','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
