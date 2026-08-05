fced892d05a3d7204e27858548775fc994dc4715
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-08-05 21:01:41 +0900

    V151.1.20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v151-1-19-video-controls';
+const CACHE_NAME = 'settertheory-v151-1-20-video-controls';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=151190','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
