60c1d852efb3cec4bde865f88843b2dba4c72645
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-08-13 08:58:13 +0900

    V151.1.52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settertheory-v151-1-51-toss-practice-growth';
+const CACHE_NAME = 'settertheory-v151-1-52-toss-practice-growth';
 const ASSETS=['./','./index.html','./app_v141_setter_split.js?v=151151','./manifest.json','./icons/aquila-192.png','./icons/aquila-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)));await self.clients.claim();})());});
